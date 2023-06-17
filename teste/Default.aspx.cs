faad64b5b2592f496cd8aa23efa729b3e15955d3
Caio Milani <67275692+caiochibantemilani@users.noreply.github.com>
2023-06-17 14:02:45 -0300

    Add files via upload

---
+++ teste/Default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Services;
+
+public partial class _Default : System.Web.UI.Page
+{
+    [WebMethod]
+    public static string MinhaFuncao()
+    {
+        // Lógica da função em C#
+        // Você pode retornar um valor, se necessário
+        return "Função executada com sucesso!";
+    }
+}
